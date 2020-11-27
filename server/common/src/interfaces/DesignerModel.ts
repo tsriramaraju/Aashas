898c0008d3e775defa3597b2f610c952f2547431
sriram <tsriramaraju@gmail.com>
2020-11-27 21:15:17 +0530

    added Designer and sales banner model

---
+++ server/common/src/interfaces/DesignerModel.ts
@@ -0,0 +1,50 @@
+import { Types, Model, Document } from 'mongoose';
+
+/**
+ * An interface that describes the properties
+ * that are required to create a new Designer Account
+ */
+interface designerAttrs {
+  name: string;
+  email: string;
+  password: string;
+  mobile: number;
+  image: string;
+  bio: string;
+  blogs: {
+    title: string;
+    image: string;
+    content: string;
+    slug: string;
+  }[];
+}
+
+/**
+ * An interface that describes the properties
+ * that a Designer Model has
+ */
+interface DesignerModel extends Model<DesignerDoc> {
+  build(attrs: designerAttrs): DesignerDoc;
+}
+
+/**
+ * An interface that describes the properties
+ * hat a Designer Document has
+ */
+interface DesignerDoc extends Document {
+  id: Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  mobile: number;
+  image: string;
+  bio: string;
+  blogs: {
+    title: string;
+    image: string;
+    content: string;
+    slug: string;
+  }[];
+}
+
+export { designerAttrs, DesignerDoc, DesignerModel };
---
+++ server/common/src/interfaces/SalesBannerModel.ts
@@ -0,0 +1,35 @@
+import { Types, Model, Document } from 'mongoose';
+
+/**
+ * An interface that describes the properties
+ * that are required to create a new Sales banner
+ */
+interface salesBannerAttrs {
+  id: Types.ObjectId;
+  title: string;
+  img: string;
+  type: 'percentage' | 'price';
+  discount: number;
+}
+
+/**
+ * An interface that describes the properties
+ * that a Sales banner  Model has
+ */
+interface SalesBannerModel extends Model<SalesBannerDoc> {
+  build(attrs: salesBannerAttrs): SalesBannerDoc;
+}
+
+/**
+ * An interface that describes the properties
+ * hat a Sales banner Document has
+ */
+interface SalesBannerDoc extends Document {
+  id: Types.ObjectId;
+  title: string;
+  img: string;
+  type: 'percentage' | 'price';
+  discount: number;
+}
+
+export { salesBannerAttrs, SalesBannerDoc };
