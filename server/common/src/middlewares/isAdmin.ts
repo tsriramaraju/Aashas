3b41612c3a26242306475b69eda56cce7eb506d6
sriram <tsriramaraju@gmail.com>
2020-11-29 16:01:54 +0530

    minor fixes

--- server/common/src/middlewares/isAdmin.ts
+++ server/common/src/middlewares/isAdmin.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/notAuthorizedError';
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.currentUser?.isAdmin) {
+  if (!req.currentUser?.isAdmin) {
     throw new NotAuthorizedError();
   }
 
