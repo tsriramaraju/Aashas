4ec93737a99bdb4f8f57d5dac14bce1f99b6f1bd
sriram <tsriramaraju@gmail.com>
2020-12-03 20:28:03 +0530

    minor fixes

--- server/common/src/interfaces/CustomProductsModel.ts
+++ server/common/src/interfaces/CustomProductsModel.ts
@@ -1,14 +1,38 @@
 import { Model, Types, Document } from 'mongoose';
 import { size } from '..';
+import { verification } from './enums';
 import { outfit, productAttrs } from './ProductsModel';
 
 /**
  * An interface that describes the properties
  * that are required to create a new Custom Product model
  */
-interface customProductsAttrs extends productAttrs {
+interface customProductsAttrs {
   refImages: String[];
   userId?: Types.ObjectId;
+
+  title: string;
+  description: string;
+  size: size[];
+  price: number;
+  color: string;
+
+  outfit: outfit;
+
+  gender: 'male' | 'female';
+  images: string[];
+}
+interface customProductRequestAttrs {
+  refImages: String[];
+  userId?: Types.ObjectId;
+  title?: string;
+  description?: string;
+  size?: size[];
+  price?: number;
+  color?: string;
+  outfit?: outfit;
+  gender?: 'male' | 'female';
+  images?: string[];
 }
 
 /**
@@ -17,6 +41,7 @@ interface customProductsAttrs extends productAttrs {
  */
 interface CustomProductModel extends Model<CustomProductDoc> {
   build(attrs: customProductsAttrs): CustomProductDoc;
+  request(attrs: customProductRequestAttrs): CustomProductDoc;
   findByEvent(event: {
     id: Types.ObjectId;
     version: number;
@@ -30,23 +55,22 @@ interface CustomProductModel extends Model<CustomProductDoc> {
 interface CustomProductDoc extends Document {
   id: Types.ObjectId;
   userId: Types.ObjectId;
+  status: verification;
   title: string;
   description: string;
   size: size[];
   price: number;
   color: string;
-  quantity: number;
   version: number;
   refImages: String[];
   outfit: outfit;
-  keywords: string[];
   gender: 'male' | 'female';
   images: string[];
-  discount: number;
-  inOffer: boolean;
-  isNewProduct: boolean;
-  designerCollection: boolean;
-  trending: boolean;
 }
 
-export { CustomProductDoc, CustomProductModel, customProductsAttrs };
+export {
+  CustomProductDoc,
+  CustomProductModel,
+  customProductRequestAttrs,
+  customProductsAttrs,
+};
