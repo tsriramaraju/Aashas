4248e086706ea8bc6aff02ff64a37162e9fea6df
tsriramaraju <tsriramaraju@gmail.com>
2020-12-04 15:34:18 +0530

    fixes

---
+++ server/notification/src/events/listeners/generateOTP.ts
@@ -0,0 +1,39 @@
+import { GenerateOTPEvent, Listener, Subjects } from '@aashas/common';
+import { Message } from 'node-nats-streaming';
+import { emailNotification } from '../../services/emailNotification';
+import { mobileNotification } from '../../services/mobileNotification';
+import { pushNotification } from '../../services/pushNotification';
+import { queueGroupName } from '../queueGroupName';
+
+export class GenerateOTPListener extends Listener<GenerateOTPEvent> {
+  queueGroupName = queueGroupName;
+  readonly subject = Subjects.GenerateOTP;
+
+  async onMessage(data: GenerateOTPEvent['data'], msg: Message) {
+    const notificationData = data.data;
+
+    data.mode.forEach((mode) => {
+      if (mode === 'email') {
+        emailNotification({
+          email: notificationData.email!,
+          body: notificationData.body!,
+          subject: notificationData.title,
+        });
+      }
+      if (mode === 'mobile') {
+        mobileNotification({
+          mobile: notificationData.mobile!,
+          message: notificationData.body!,
+        });
+      }
+      if (mode === 'push notification') {
+        pushNotification({
+          id: data.clientID!,
+          message: notificationData.body!,
+        });
+      }
+    });
+
+    msg.ack();
+  }
+}
