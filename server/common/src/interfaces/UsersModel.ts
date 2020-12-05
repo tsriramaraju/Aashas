b06a955b8d4144560806a9edd769167cc29cd6fc
sriram <tsriramaraju@gmail.com>
2020-12-05 12:40:55 +0530

    minor fixes

--- server/common/src/interfaces/UsersModel.ts
+++ server/common/src/interfaces/UsersModel.ts
@@ -60,10 +60,10 @@ interface UserDoc extends Document {
   authType: authType;
   addresses?: address[];
   defaultAddress?: address;
-  cart?: ProductDoc[] | string;
-  orders?: ProductDoc[] | string;
-  favourites?: ProductDoc[] | string;
-  customProducts?: CustomProductDoc[] | string;
+  cart?: ProductDoc[] | string[];
+  orders?: ProductDoc[] | string[];
+  favourites?: ProductDoc[] | string[];
+  customProducts?: CustomProductDoc[] | string[];
 }
 
 export { userAttrs, UserDoc, UserModel, address };
