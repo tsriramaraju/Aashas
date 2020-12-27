c750326af8b655e2f435c3a35752a555e8db3af5
sriram <tsriramaraju@gmail.com>
2020-12-27 15:43:41 +0530

    done layout

--- webApp/src/components/svg/icons/user.tsx
+++ webApp/src/components/svg/icons/user.tsx
@@ -5,44 +5,52 @@ type UserProps = {
   stroke: string;
   className?: string;
   changeNav: (nav: string) => void;
+  location: string;
 };
 
-export function User({ stroke, className, changeNav }: UserProps) {
+export function User({ stroke, className, changeNav, location }: UserProps) {
   return (
     <motion.div className={className} onClick={() => changeNav('user')} animate>
-      <motion.svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 106.12 118.63"
-        stroke={stroke}
-        className={style.icon}
-        animate
+      <motion.div
+        className={location === 'user' ? style.menuSelected : style.menu}
       >
-        <motion.g id="Layer_2" data-name="Layer 2">
-          <motion.g id="Layer_1-2" data-name="Layer 1">
-            <motion.g
-              id="User"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="6px"
-            >
-              <motion.path
-                className="cls-1"
-                d="M103.12,115.63V103.12a25,25,0,0,0-25-25H28a25,25,0,0,0-25,25v12.51"
-              />
-              <motion.circle
-                className="cls-1"
-                cx="53.06"
-                cy="28.03"
-                r="25.03"
-              />
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 106.12 118.63"
+          stroke={stroke}
+          className={style.icon}
+          animate
+        >
+          <motion.g id="Layer_2" data-name="Layer 2">
+            <motion.g id="Layer_1-2" data-name="Layer 1">
+              <motion.g
+                id="User"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="6px"
+              >
+                <motion.path
+                  className="cls-1"
+                  d="M103.12,115.63V103.12a25,25,0,0,0-25-25H28a25,25,0,0,0-25,25v12.51"
+                />
+                <motion.circle
+                  className="cls-1"
+                  cx="53.06"
+                  cy="28.03"
+                  r="25.03"
+                />
+              </motion.g>
             </motion.g>
           </motion.g>
-        </motion.g>
-      </motion.svg>
-      <motion.p animate className={style.text}>
-        User
-      </motion.p>
+        </motion.svg>
+        <motion.p animate className={style.text}>
+          User
+        </motion.p>
+      </motion.div>
+      {location === 'user' && (
+        <motion.div layoutId="border" className={style.border} />
+      )}
     </motion.div>
   );
 }
--- webApp/src/layout/mobileNav/MobileNav.tsx
+++ webApp/src/layout/mobileNav/MobileNav.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Custom, Home, Icon, Shop, User } from '../../components/svg';
 
 import styles from './mobileNav.module.scss';
-import { AnimateSharedLayout } from 'framer-motion';
+import { AnimateSharedLayout, motion } from 'framer-motion';
+
 const MobileNav = () => {
   const [nav, setNav] = useState('home');
 
@@ -12,29 +13,34 @@ const MobileNav = () => {
 
   return (
     <AnimateSharedLayout>
-      <section className={styles.nav}>
+      <motion.div animate className={styles.nav}>
         <Home
           className={nav === 'home' ? styles.selected : styles.item}
           changeNav={changNav}
+          location={nav}
         />
         <Shop
           className={nav === 'shop' ? styles.selected : styles.item}
           changeNav={changNav}
+          location={nav}
         />
         <Custom
           className={nav === 'custom' ? styles.selected : styles.item}
           changeNav={changNav}
+          location={nav}
         />
         <Icon
           className={nav === 'aashas' ? styles.selected : styles.item}
           changeNav={changNav}
+          location={nav}
         />
         <User
           stroke="#47556E"
           className={nav === 'user' ? styles.selected : styles.item}
           changeNav={changNav}
+          location={nav}
         />
-      </section>
+      </motion.div>
     </AnimateSharedLayout>
   );
 };
